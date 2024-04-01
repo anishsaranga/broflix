8ffc438b6e7f41e57f33d9ed163ebb257c47529c
anishsaranga <anishsaranga@gmail.com>
2024-04-01 09:52:57 -0400

    home page text and border

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,6 +7,16 @@ function Home() {
     {/* Featured banner */}
     {/* CategoryItems x3-4 */}
     {/* <CategoryItems category={"upcoming"} /> */}
+    <div className="hidden md:block text-center my-10 mx-auto p-4">
+      <div className='font-extralight text-2xl text-stone-200 mb-4'>Watch</div>
+      <div className='font-bold text-4xl text-white'>Unlimited Movies and TV</div>
+      <div className='w-full h-9 p-15 mt-auto border-b-4 border-red-600 opacity-45 rounded-full'></div>
+    </div>
+    <div className="md:hidden text-white text-center my-8 mx-auto p-2">
+      <div className='font-extralight text-xl text-stone-200 mb-3'>Watch</div>
+      <div className='font-bold text-3xl text-white'>Unlimited Movies and TV</div>
+      <div className='w-full h-1 p-10 mt-auto border-b-4 border-red-600 opacity-45 rounded-full'></div>
+    </div>
     <CategoryItems category={"popular"} />
     <CategoryItems category={"top_rated"} />
     <CategoryItems category={"trending"} />
