6dea57b55def8a4c2443027b05301b9f22564c84
anishsaranga <anishsaranga@gmail.com>
2024-04-05 23:09:02 -0400

    tv-working

--- src/components/StreamPage.jsx
+++ src/components/StreamPage.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
+import { bearer_token } from "../API_KEY";
 
 function StreamPage() {
   const location = useLocation();
@@ -8,24 +9,92 @@ function StreamPage() {
   const isMovie = itemObj.title ? true : false;
   // vid url
   const vidUrl = `https://vidsrc.xyz/embed/`;
-  // season
-  const [season, setSeason] = useState(1);
-  const [episode, setEpisode] = useState(1);
   // imdb id
   const imdbIdObj = useParams();
 
+  // api for season and episode details
+  const tvApiUrl = `https://api.themoviedb.org/3/tv/${itemObj.id}`;
+  // season
+  const [seasonsCount, setSeasonsCount] = useState(1);
+  const [episodesCount, setEpisodesCount] = useState(1);
+  const [seriesData, setSeriesData] = useState(null);
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${bearer_token}`,
+    },
+  };
+
+  useEffect(() => {
+    if (!isMovie) {
+      fetch(tvApiUrl, options)
+        .then((response) => response.json())
+        .then((data) => data && setSeriesData(data));
+    }
+  }, []);
+
+  // setting the number of seasons in case of tv show
+  useEffect(() => {
+    seriesData && setSeasonsCount(seriesData["seasons"].length);
+  }, [seriesData]);
+  // setting episodes
+  useEffect(() => {
+    seriesData &&
+      setEpisodesCount(
+        seriesData["seasons"][seasonsCount - 1]["episode_count"]
+      );
+  }, [seasonsCount]);
+
+  // set final season and episode
+  const [selectedSeason, setSelectedSeason] = useState(1);
+  const [selectedEpisode, setSelectedEpisode] = useState(1);
+
+  // get 1-n array
+  function createArray(N) {
+    return Array.from({ length: N }, (_, index) => index + 1);
+  }
+
   return (
     <>
-      <div className="text-center items-center text-stone-200 m-2 p-1 w-full h-[80vh]">
+      <div className="text-center items-center m-2 p-1 w-full h-[80vh]">
         {/* title */}
-        <span className="text-2xl md:text-4xl my-4 shadow-md">
+        <span className="text-2xl md:text-4xl my-4 shadow-md text-stone-200">
           {itemObj.title ? itemObj.title : itemObj.name}
         </span>
         {/* if tv show, put season and episode details */}
-        {itemObj.name && (
-          <div className="flex flex-row justify-items-center items-center text-stone-200">
-            <div></div>
-            <div></div>
+        {!isMovie && (
+          <div className="flex flex-row justify-center items-center">
+            {/* season */}
+            <label className="m-2">
+              <span className="text-stone-200">{"Season\t\t"}</span>
+              <select
+                value={selectedSeason}
+                onChange={(e) => setSelectedSeason(e.target.value)}
+              >
+                {seasonsCount &&
+                  createArray(seasonsCount).map((season) => (
+                    <option key={season} value={season}>
+                      {season}
+                    </option>
+                  ))}
+              </select>
+            </label>
+            {/* episode */}
+            <label className="m-2">
+              <span className="text-stone-200">{"Episode\t\t"}</span>
+              <select
+                value={selectedEpisode}
+                onChange={(e) => setSelectedEpisode(e.target.value)}
+              >
+                {episodesCount &&
+                  createArray(episodesCount).map((episode) => (
+                    <option key={episode} value={episode}>
+                      {episode}
+                    </option>
+                  ))}
+              </select>
+            </label>
           </div>
         )}
         {/* embed vid */}
@@ -36,7 +105,14 @@ function StreamPage() {
             allowFullScreen
           ></iframe>
         ) : (
-          <iframe className="h-full w-full my-5 mx-auto" src=""></iframe>
+          <iframe
+            className="h-full w-full my-5 mx-auto"
+            src={
+              vidUrl +
+              `tv?imdb=${imdbIdObj.imdbIdObj}&season=${selectedSeason}&episode=${selectedEpisode}`
+            }
+            allowFullScreen
+          ></iframe>
         )}
       </div>
     </>
